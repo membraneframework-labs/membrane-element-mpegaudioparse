6a239c91e10a4ac98674570167c225a1d17fe3c9
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2017-09-21 13:23:36 +0200

    Skip bytes until a valid frame

--- lib/membrane_element_mpegaudioparse/parser.ex
+++ lib/membrane_element_mpegaudioparse/parser.ex
@@ -1,5 +1,6 @@
 defmodule Membrane.Element.MPEGAudioParse.Parser do
   use Membrane.Element.Base.Filter
+  use Membrane.Mixins.Log, tags: :membrane_element_mpegaudioparse
   alias Membrane.Caps.Audio.MPEG
 
   @mpeg_header_size 4
@@ -25,12 +26,13 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
   end
 
   def handle_demand(:source, n_bufs, :buffers, _params, %{queue: queue, frame_size: frame_size} = state) do
-    demanded_bytes = frame_size * n_bufs - byte_size(queue)
+    demanded_bytes = frame_size * n_bufs - byte_size(queue) + @mpeg_header_size
     {{:ok, demand: {:sink, demanded_bytes}}, state}
   end
 
   def handle_process1(:sink, %Membrane.Buffer{payload: payload}, _params, %{queue: queue, caps: caps, frame_size: frame_size} = state) do
-    case do_parse(queue <> payload, caps, frame_size, []) do
+    data = (queue <> payload) |> skip_to_frame()
+    case do_parse(data, caps, frame_size, []) do
       {:ok, commands, new_queue, new_caps, new_frame_size} ->
         {{:ok, commands |> Enum.reverse}, %{state | queue: new_queue, caps: new_caps, frame_size: new_frame_size}}
     end
@@ -39,7 +41,8 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
   defp do_parse(<< >>, previous_caps, prev_frame_size, acc), do: {:ok, acc, << >>, previous_caps, prev_frame_size}
 
   # We have at least header.
-  defp do_parse(payload, previous_caps, _prev_frame_size, acc) when byte_size(payload) >= @mpeg_header_size do
+  defp do_parse(payload, previous_caps, prev_frame_size, acc) when byte_size(payload) >= @mpeg_header_size do
+    IO.puts(:stderr, "do_parse: #{inspect payload}")
     << 0b11111111111   :: size(11),
        version         :: 2-bitstring,
        layer           :: 2-bitstring,
@@ -79,26 +82,36 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
         emphasis_mode: parse_emphasis_mode(emphasis_mode),
       }
 
-    frame_size = if padding_enabled do
-      ((144 * bitrate * 1000) |> div(sample_rate)) + 1
+    invalid = caps |> Map.values |> Enum.any?(fn val -> val in [:invalid, :free] end)
+
+    if invalid do
+        do_parse(force_skip_to_frame(payload), previous_caps, prev_frame_size, acc)
     else
-      (144 * bitrate * 1000) |> div(sample_rate)
-    end
-
-    if byte_size(payload) < frame_size do
-      {:ok, acc, payload, previous_caps, frame_size}
-
-    else
-      << frame_payload :: size(frame_size)-binary, rest :: bitstring >> = payload
-
-      acc = if previous_caps != caps do
-        [{:caps, {:source, caps}}|acc]
+      frame_size = if padding_enabled do
+        ((144 * bitrate * 1000) |> div(sample_rate)) + 1
       else
-        acc
+        (144 * bitrate * 1000) |> div(sample_rate)
       end
 
-      frame_buffer = {:buffer, {:source, %Membrane.Buffer{payload: frame_payload}}}
-      do_parse(rest, caps, frame_size, [frame_buffer|acc])
+      if byte_size(payload) < frame_size do
+        {:ok, acc, payload, previous_caps, frame_size}
+      else
+        << frame_payload :: size(frame_size)-binary, rest :: bitstring >> = payload
+        if byte_size(rest) < 11 or pre_check(rest) do
+          acc = if previous_caps != caps do
+            [{:caps, {:source, caps}}|acc]
+          else
+            acc
+          end
+
+          frame_buffer = {:buffer, {:source, %Membrane.Buffer{payload: frame_payload}}}
+          do_parse(rest, caps, frame_size, [frame_buffer|acc])
+        else
+          # Next frame is not valid, so we got the frame size wrong. It means that we parsed random data
+          # that fitted MPEG Audio Header by accident. Let's look for another frame.
+          payload |> force_skip_to_frame |> do_parse(previous_caps, prev_frame_size, acc)
+        end
+      end
     end
   end
 
@@ -106,15 +119,39 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
     {:ok, acc, payload, previous_caps, prev_frame_size}
   end
 
+  defp pre_check(<< 0b11111111111 :: size(11), _ :: bitstring >>), do: true
+  defp pre_check(_), do: false
+
+  defp force_skip_to_frame(<< >>), do: << >>
+  defp force_skip_to_frame(payload) do
+    payload |> binary_part(1, byte_size(payload) - 1) |> skip_to_frame
+  end
+
+  defp skip_to_frame(<< >>), do: << >>
+  defp skip_to_frame(<< 0b11111111111 :: size(11), _ :: bitstring >> = frame), do: frame
+  defp skip_to_frame(payload) do
+    # Skip one byte to avoid infinite loop
+    next_payload = payload |> binary_part(1, byte_size(payload) - 1)
+    size = byte_size(next_payload)
+    case next_payload |> :binary.match(<<0xff>>) do
+      {pos, _len} ->
+        IO.puts(:stderr, inspect binary_part(payload, 0, pos + 1))
+        next_payload |> binary_part(pos, size - pos)
+      :nomatch   -> << >>
+    end
+    |> skip_to_frame()
+  end
 
   defp parse_version(<< 0b00 :: size(2) >>), do: :v2_5
   defp parse_version(<< 0b10 :: size(2) >>), do: :v2
   defp parse_version(<< 0b11 :: size(2) >>), do: :v1
+  defp parse_version(_), do: :invalid
 
 
   defp parse_layer(<< 0b01 :: size(2) >>), do: :layer3
   defp parse_layer(<< 0b10 :: size(2) >>), do: :layer2
   defp parse_layer(<< 0b11 :: size(2) >>), do: :layer1
+  defp parse_layer(_), do: :invalid
 
 
   defp parse_crc_enabled(<< 1 :: 1>>), do: true
@@ -122,7 +159,6 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
 
 
   defp parse_bitrate(<< 0b0000 :: size(4) >>, _, _), do: :free
-  defp parse_bitrate(<< 0b1111 :: size(4) >>, _, _), do: :bad
 
   defp parse_bitrate(<< 0b0001 :: size(4) >>, :v1, :layer1), do: 32
   defp parse_bitrate(<< 0b0010 :: size(4) >>, :v1, :layer1), do: 64
@@ -259,6 +295,7 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
   defp parse_bitrate(<< 0b1101 :: size(4) >>, :v2_5, :layer3), do: 144
   defp parse_bitrate(<< 0b1110 :: size(4) >>, :v2_5, :layer3), do: 160
 
+  defp parse_bitrate(_, _, _), do: :invalid
 
   defp parse_sample_rate(<< 0b00 :: size(2) >>, :v1), do: 44100
   defp parse_sample_rate(<< 0b01 :: size(2) >>, :v1), do: 48000
@@ -271,6 +308,7 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
   defp parse_sample_rate(<< 0b00 :: size(2) >>, :v2_5), do: 11050
   defp parse_sample_rate(<< 0b01 :: size(2) >>, :v2_5), do: 12000
   defp parse_sample_rate(<< 0b10 :: size(2) >>, :v2_5), do: 8000
+  defp parse_sample_rate(_, _), do: :invalid
 
 
   defp parse_padding_enabled(<< 1 :: 1 >>), do: true
@@ -285,6 +323,7 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
   defp parse_channel_mode(<< 0b01 :: size(2) >>), do: :joint_stereo
   defp parse_channel_mode(<< 0b10 :: size(2) >>), do: :dual_channel
   defp parse_channel_mode(<< 0b11 :: size(2) >>), do: :single_channel
+  defp parse_channel_mode(_), do: :invalid
 
 
   defp parse_mode_extension(_, :stereo), do: nil
@@ -295,6 +334,7 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
   defp parse_mode_extension(<< 0b01 :: size(2) >>, :joint_stereo), do: :mode1
   defp parse_mode_extension(<< 0b10 :: size(2) >>, :joint_stereo), do: :mode2
   defp parse_mode_extension(<< 0b11 :: size(2) >>, :joint_stereo), do: :mode3
+  defp parse_mode_extension(_, _), do: :invalid
 
 
   defp parse_original(<< 1 :: 1 >>), do: true
@@ -308,4 +348,5 @@ defmodule Membrane.Element.MPEGAudioParse.Parser do
   defp parse_emphasis_mode(<< 0b00 :: size(2) >>), do: :none
   defp parse_emphasis_mode(<< 0b01 :: size(2) >>), do: :emphasis_50_15
   defp parse_emphasis_mode(<< 0b11 :: size(2) >>), do: :ccit_j_17
+  defp parse_emphasis_mode(_), do: :invalid
 end
