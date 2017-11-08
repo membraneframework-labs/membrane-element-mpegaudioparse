e0516f04159355234b9b6c0285086c6409922bd5
Mateusz Front <mateusz.front@swmansion.com>
2017-11-08 17:48:07 +0100

    set core version to v0.2

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Membrane.Element.MPEGAudioParse.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, git: "git@github.com:membraneframework/membrane-core.git", branch: "feature/pull"},
+      {:membrane_core, git: "git@github.com:membraneframework/membrane-core.git", branch: "v0.2"},
       {:membrane_caps_audio_mpeg, git: "git@github.com:membraneframework/membrane-caps-audio-mpeg.git"},
 
       {:espec,       "~> 1.4.6", only: :test},
