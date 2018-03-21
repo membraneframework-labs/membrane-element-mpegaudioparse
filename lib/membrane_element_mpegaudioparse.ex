0eafd092d051dbc514dd51862f024ea447807749
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-03-21 10:30:10 +0100

    Format the code and bump deps versions

--- lib/membrane_element_mpegaudioparse.ex
+++ lib/membrane_element_mpegaudioparse.ex
@@ -4,9 +4,7 @@ defmodule Membrane.Element.MPEGAudioParse do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [
-
-    ]
+    children = []
 
     opts = [strategy: :one_for_one, name: Membrane.Element.MPEGAudioParse]
     Supervisor.start_link(children, opts)
