c2b118ecf786deb18837fc93acd84677208df3c9
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-11-06 13:19:52 +0100

    Bump version to v0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.MPEGAudioParse.Mixfile do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.2.0"
   @github_url "https://github.com/membraneframework/membrane-element-mpegaudioparse"
 
   def project do
